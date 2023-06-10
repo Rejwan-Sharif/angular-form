329d21db84ad07e6d75811768def240c613b97bc
Rejwan Sharif <mohammedrejwan8@gmail.com>
2023-06-10 19:07:40 +0600

    crud operation with jason server and routing mod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Route, Router, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const AppRoutes:Routes=[
+  {path:'home', component:HomeComponent},
+  {path:'about', component:AboutComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
